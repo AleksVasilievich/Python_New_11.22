48850aa13c8514ad5db522b0c567c16abf1c8f68
AleksVasilievich <104069090+AleksVasilievich@users.noreply.com>
2022-11-18 09:37:25 +0300

    Вариант 3

--- Python_seminar_7/logger.py
+++ Python_seminar_7/logger.py
@@ -1,11 +1,6 @@
 import view as v
 
-# def mod_1():
-#     ls = v.ponebook().split(',')
-#     return ','.join(ls)
-# # print(mod_1())
 def mod_1():
-    # ls = v.ponebook().split(',')
     return ','.join(v.ponebook().split(','))
 # print(mod_1())
 
