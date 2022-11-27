fa8f985d91d21135c000db8777b4fdabc62663ca
AleksVasilievich <104069090+AleksVasilievich@users.noreply.com>
2022-11-27 22:52:15 +0300

    add

--- Python_seminar_9/file.py
+++ Python_seminar_9/file.py
@@ -1,15 +1,17 @@
 from telegram import Bot
 from telegram.ext import Updater, CommandHandler
 
-bot = Bot(token = '')
-updater = Updater(token = '')
-dispatcher = updater.dispatcher
+bot = Bot(token = '')                     # Создаём бота
+updater = Updater(token = '')     # Здесь будут хронится все обновления нашего чата,имя id и вся ифа.
+dispatcher = updater.dispatcher   # Переменная которая будет хранить всё что в updater 
 
-def start(update, context):
-    context.bot.send_message(update.effective_chat.id, "Hello")
+def start(update, context):        # Функция - (update и context - всегда ! ) 1 - инфа о чате, 2 - встроинные команды 
+    context.bot.send_message(update.effective_chat.id, "Hello")     # команда ответа бота
 
-start_handler = CommandHandler("start", start)
+start_handler = CommandHandler("start", start)     # Если человек боту напишет "/start", то он запустит функцию старт
+                                                   # Функция выполница, бот ответит "Hello"
+                                                   
+dispahather.add_handler(start_handler)             # Обучаем бота добовленным фунциям, которые мы прописали ранее
 
-dispahather.add_handler(start_handler)
-updater.start_polling()
-updater.idle()
+updater.start_polling()      # Старт программы
+updater.idle()               # Конец программы
