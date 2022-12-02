42e040d06b5cc8031e15488fdfb2ddbd46b8b2bb
AleksVasilievich <104069090+AleksVasilievich@users.noreply.com>
2022-12-02 11:55:40 +0300

    Вариант_3

--- Python_Task_10_1/main.py
+++ Python_Task_10_1/main.py
@@ -2,40 +2,6 @@ from telegram import Bot
 from telegram.ext import Updater, CommandHandler, Filters, MessageHandler, ConversationHandler
 from controller import line, calaulate 
 from log import id_user, input_data, results, write_log
-# from log import id_user, input_data, results,write_log
-# from controller import line, calaulate
-# bot = Bot(token = '5548072895:AAFZOcvGv2XKPHcv_vikG0LLVxvscO9vnQw')                    
-# updater = Updater(token = '5548072895:AAFZOcvGv2XKPHcv_vikG0LLVxvscO9vnQw')     
-# dispatcher = updater.dispatcher   
-
-# def start(update, context):        
-#     context.bot.send_message(update.effective_chat.id, 'start')    
-#     context.bot.send_message(update.effective_chat.id, 'Напишите мне арифметическое выражение ')
-#     id_user(update.effective_chat.id)
-
-# # def text_task(update, context):
-# #     text = update.message.text
-# #     input_data(text)
-# #     data = line(st)
-# #     result = callable(data)
-    
-#     context.bot.send_message(update.effective_chat.id, f'Результат: {result}')
-
-#     # return ConversationHandler.END
-
-# start_handler = CommandHandler('start', start)    
-                                                
-
-# # text_task_handler = MessageHandler(Filters.text, text_task)
-
-
-
-# dispatcher.add_handler(start_handler)            
-
-# # dispatcher.add_handler(text_task_handler)
-
-# updater.start_polling()     
-# updater.idle()              
 
 
 bot = Bot(token = '5548072895:AAFZOcvGv2XKPHcv_vikG0LLVxvscO9vnQw')                     # Создаём бота
@@ -44,7 +10,7 @@ dispatcher = updater.dispatcher   # Переменная которая буде
 
 def start(update, context):        # Функция - (update и context - всегда ! ) 1 - инфа о чате, 2 - встроинные команды 
     context.bot.send_message(update.effective_chat.id, 'Hello')     # команда ответа бота
-    context.bot.send_message(update.effective_chat.id, 'Напишите мне, я возьму слова где нет "абв"')
+    context.bot.send_message(update.effective_chat.id, 'Давай посчитаем !')
     id_user(update.effective_chat.id)
 
 def text_filtr(update, context):
@@ -59,14 +25,11 @@ def text_filtr(update, context):
 
 
 start_handler = CommandHandler('start', start)     # Если человек боту напишет '/start', то он запустит функцию старт
-                                                   
-
 text_handler = MessageHandler(Filters.text, text_filtr)
 
-
 dispatcher.add_handler(start_handler)             # Обучаем бота добовленным фунциям, которые мы прописали ранее
-
 dispatcher.add_handler(text_handler)
+
 
 updater.start_polling()      # Старт программы
 updater.idle()               # Конец программы
